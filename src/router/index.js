c16df7725a0e270222940bf97080d16b8e4c6043
ZHAO ZIPENG <72734462+JariusF10-8@users.noreply.github.com>
2026-03-29 14:46:32 +0800

    feat[index.js]: State persistence
    Implemented state persistence (after user logs in, they will remain logged in for 7 days, and /login auto redirects to /grand for a user that has previously logged in)

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import Login from '../login/Login.vue'
 import Register from '../login/Register.vue'
 import ResetPassword from '../login/Reset-Password.vue'
@@ -36,8 +37,31 @@ const router = createRouter({
       path: '/grand',
       name: 'grand',
       component: Grand,
+      meta: { requiresAuth: true }
     },
   ],
+})
+
+function getCurrentUser() {
+  return new Promise((resolve, reject) => {
+    const unsubscribed = onAuthStateChanged(getAuth(), user => { unsubscribed(); resolve(user) }, reject);
+  })
+}
+
+// Route guard to check for authenticated access for routes that require authentication
+
+router.beforeEach(async (to, from, next) => {
+  const user = await getCurrentUser();
+
+  if (to.meta.requiresAuth && !user) {
+    next('/login');
+  }
+
+  if (to.path === '/login' && user) {
+    next('/grand');
+  }
+
+  next();
 })
 
 export default router
