87560f85d71ceffcc2dd9043afb25e9d564dae27
Wang-Chuhao <156888781+Wang-Chuhao@users.noreply.github.com>
2026-03-30 18:44:26 +0800

    Update Settings.vue to change menu items

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -7,6 +7,27 @@
         <main class="page-content">
             <p class="desc">Preferences</p>
             <div class="content settings-group">
+                <button class="menu-item" @click="$router.push('/goals')">
+                    <div class="menu-left">
+                        <div class="menu-icon-circle">
+                            <svg class="menu-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="12" cy="12" r="9" stroke="currentColor" stroke-width="2"></circle>
+                                <circle cx="12" cy="12" r="5" stroke="currentColor" stroke-width="2"></circle>
+                                <circle cx="12" cy="12" r="1" fill="currentColor"></circle>
+                            </svg>
+                        </div>
+
+                        <div class="menu-text">
+                            <p class="menu-title">Goals</p>
+                            <p class="menu-subtext">Set and track your spending targets</p>
+                        </div>
+                    </div>
+
+                    <svg class="menu-arrow" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M10 7L15 12L10 17" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg>
+                </button>
+
                 <button class="menu-item">
                     <div class="menu-left">
                         <div class="menu-icon-circle">
@@ -28,130 +49,14 @@
                 </button>
 
                 <button class="menu-item">
-                    <div class="menu-left">
-                        <div class="menu-icon-circle">
-                            <svg class="menu-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M12 6C9.8 6 8 7.8 8 10V11L6.5 13.5V14H17.5V13.5L16 11V10C16 7.8 14.2 6 12 6Z" stroke="currentColor" stroke-width="1.8" stroke-linejoin="round"></path>
-                                <path d="M10.5 17C10.8 17.6 11.3 18 12 18C12.7 18 13.2 17.6 13.5 17" stroke="currentColor" stroke-width="1.8" stroke-linecap="round"></path>
-                            </svg>
-                        </div>
-
-                        <div class="menu-text">
-                            <p class="menu-title">Notifications</p>
-                            <p class="menu-subtext">Push notifications and alerts</p>
-                        </div>
-                    </div>
-
-                    <svg class="menu-arrow" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10 7L15 12L10 17" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"></path>
-                    </svg>
-                </button>
+                    </button>
             </div>
             
-            <p class="desc">Account</p>
-            <div class="content settings-group">
-                <button class="menu-item" @click="$router.push('/profile')">
-                    <div class="menu-left">
-                        <div class="menu-icon-circle">
-                            <svg class="menu-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                            </svg>
-                        </div>
-
-                        <div class="menu-text">
-                            <p class="menu-title">Profile</p>
-                            <p class="menu-subtext">Personal Information and preferences</p>
-                        </div>
-                    </div>
-
-                    <svg class="menu-arrow" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10 7L15 12L10 17" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"></path>
-                    </svg>
-                </button>
-
-                <button class="menu-item">
-                    <div class="menu-left">
-                        <div class="menu-icon-circle">
-                            <svg class="menu-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M5 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2v-6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                <path d="M11 16a1 1 0 1 0 2 0a1 1 0 0 0 -2 0" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                <path d="M8 11v-4a4 4 0 1 1 8 0v4" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                            </svg>
-                        </div>
-
-                        <div class="menu-text">
-                            <p class="menu-title">Security</p>
-                            <p class="menu-subtext">Password and authentication</p>
-                        </div>
-                    </div>
-
-                    <svg class="menu-arrow" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10 7L15 12L10 17" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"></path>
-                    </svg>
-                </button>
-            </div>
-            
-            <p class="desc">Support</p>
-            <div class="content settings-group">
-                <button class="menu-item">
-                    <div class="menu-left">
-                        <div class="menu-icon-circle">
-                            <svg class="why-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <circle cx="12" cy="12" r="8" stroke="currentColor" stroke-width="1.8"></circle>
-                                <path d="M9.8 9.5C9.8 8.3 10.8 7.5 12 7.5C13.2 7.5 14.2 8.3 14.2 9.5C14.2 10.4 13.7 11 12.8 11.5C12.2 11.9 12 12.2 12 13" stroke="currentColor" stroke-width="1.8" stroke-linecap="round"></path>
-                                <circle cx="12" cy="16.5" r="1" fill="currentColor"></circle>
-                            </svg>
-                        </div>
-
-                        <div class="menu-text">
-                            <p class="menu-title">Help Center</p>
-                            <p class="menu-subtext">FAQ and support articles</p>
-                        </div>
-                    </div>
-
-                    <svg class="menu-arrow" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10 7L15 12L10 17" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"></path>
-                    </svg>
-                </button>
-
-                <button class="menu-item">
-                    <div class="menu-left">
-                        <div class="menu-icon-circle">
-                            <svg class="info-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                <path d="M12 9h.01" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                                <path d="M11 12h1v4h1" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                            </svg>
-                        </div>
-
-                        <div class="menu-text">
-                            <p class="menu-title">About</p>
-                            <p class="menu-subtext">Version 1.0</p>
-                        </div>
-                    </div>
-
-                    <svg class="menu-arrow" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10 7L15 12L10 17" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"></path>
-                    </svg>
-                </button>
-            </div>
-        </main>
+            </main>
 
         <BottomNav currentTab="settings" />
     </div>
 </template>
-
-<script>
-import BottomNav from "@/components/BottomNav.vue"
-
-export default {
-    name:"Settings",
-    components: {
-        BottomNav
-    }
-}
-</script>
 
 <style scoped>
 .web-page {
